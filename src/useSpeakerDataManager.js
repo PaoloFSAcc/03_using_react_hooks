583ee3c56884d19e5f93ec4efeeea00ffb286147
Paolo <paoloagva@gmail.com>
2022-02-03 17:03:10 +0100

    Access to data through api calls

--- src/useSpeakerDataManager.js
+++ src/useSpeakerDataManager.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useReducer } from "react";
 import SpeakerData from "./SpeakerData";
 import speakersReducer from "./speakersReducer";
+import axios from "axios";
 
 function useSpeakerDataManager() {
     const [{ isLoading, speakerList }, dispatch] = useReducer(speakersReducer, {
@@ -9,23 +10,36 @@ function useSpeakerDataManager() {
     });
 
     function toggleSpeakerFavorite(speakerRec) {
-        speakerRec.favorite == true
-            ? dispatch({ type: "unfavorite", id: speakerRec.id })
-            : dispatch({ type: "favorite", id: speakerRec.id });
+        const updateData = async function () {
+            axios.put(`/api/speakers/${speakerRec.id}`, {
+                ...speakerRec,
+                favorite: !speakerRec.favorite,
+            });
+            speakerRec.favorite == true
+                ? dispatch({ type: "unfavorite", id: speakerRec.id })
+                : dispatch({ type: "favorite", id: speakerRec.id });
+        };
+        updateData();
     }
 
     useEffect(() => {
-        new Promise(function (resolve) {
-            setTimeout(function () {
-                resolve();
-            }, 1000);
-        }).then(() => {
-            //setSpeakerList(speakerListServerFilter);
-            dispatch({
-                type: "setSpeakerList",
-                data: SpeakerData,
-            });
-        });
+        // new Promise(function (resolve) {
+        //     setTimeout(function () {
+        //         resolve();
+        //     }, 1000);
+        // }).then(() => {
+        //     //setSpeakerList(speakerListServerFilter);
+        //     dispatch({
+        //         type: "setSpeakerList",
+        //         data: SpeakerData,
+        //     });
+        // });
+        const fetchData = async function () {
+            let result = await axios.get("/api/speakers");
+            dispatch({ type: "setSpeakerList", data: result.data });
+        };
+        fetchData();
+
         return () => {
             console.log("cleanup");
         };
